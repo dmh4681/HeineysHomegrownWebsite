562302bf56ffb69bbeebbe4993dec4d1ce8b59ea
dmh4681 <dmh4681@gmail.com>
2016-12-09 17:29:13 -0600

    Add files via upload

--- form-to-email.php
+++ form-to-email.php
@@ -3,11 +3,11 @@
 if(isset($_POST['submit'])){
     $to = "dylan@heineyhomegrownhealth.com"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
-    $first_name = $_POST['name'];
+    $name = $_POST['name'];
     $subject = "Form submission";
     $subject2 = "Copy of your form submission";
-    $message = $first_name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+    $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
 
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
