36f3ebcdf08ba5af4d347921920f4e73158b2cee
dmh4681 <dmh4681@gmail.com>
2016-12-09 13:37:22 -0600

    Add files via upload

--- form-to-email.php
+++ form-to-email.php
@@ -21,12 +21,12 @@ if(IsInjected($visitor_email))
     exit;
 }
 
-$email_from = 'dylan.heineytrainer@gmail.com';//<== update the email address
+$email_from = 'dylan@heineyhomegrownhealth.com';//<== update the email address
 $email_subject = "New Form submission";
 $email_body = "You have received a new message from the user $name.\n".
     "Here is the message:\n $message";
     
-$to = "dylan.heineytrainer@gmail.com";//<== update the email address
+$to = "dylan@heineyhomegrownhealth.com";//<== update the email address
 $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 //Send the email!
